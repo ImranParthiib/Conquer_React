d91ddaed7480df94c39e236c64513110e75f8121
ImranParthib <imranparthib13@gmail.com>
2024-06-08 02:29:08 +0600

    chore: Add new dependencies and configuration files for tic-tac-toe game

---
+++ tic-tac-toe/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("move", (squares) => {
+    socket.broadcast.emit("move", squares);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
