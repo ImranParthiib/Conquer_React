51e8f07f5f8d11eda9f625cdc95af273c4d2dbfb
ImranParthib <imranparthib13@gmail.com>
2024-11-10 00:13:33 +0600

    🔥 Set up React project with Vite and Tailwind CSS: add initial files, configuration, and styles

---
+++ stateManagement/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+        <h1 className="text-white">Change Light On/Off using useState Hook</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
