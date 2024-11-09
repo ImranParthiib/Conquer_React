c38ac50777a4c1cf54724755d7318bdae9e31390
ImranParthib <imranparthib13@gmail.com>
2024-11-10 00:37:48 +0600

    🔥 Refactor light toggle functionality: simplify state management, enhance UI with clickable image, and update button text

--- stateManagement/src/App.jsx
+++ stateManagement/src/App.jsx
@@ -6,24 +6,26 @@ import { useState } from "react";
 function App() {
   const [lightState, setLightState] = useState(false);
 
-  const lightStateChange = () => {
-    console.log("Light State Changed");
-    setLightState(!lightState); // Toggle the light state
-  };
   return (
     <>
       <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 p-4">
-        <h1 className="text-white mb-10 font-mono text-xl sm:text-2xl text-center">
+        <h1 className="text-white mb-10 font-mono text-xl sm:text-2xl text-center border p-1">
           Change Light On/Off using useState Hook
         </h1>
         <div className="mb-10">
-          <img src={lightState ? lightOn : lightOff} alt="Light" className="w-32 sm:w-48 md:w-64" />
+          <img
+            src={lightState ? lightOn : lightOff}
+            alt="Light"
+            className="w-32 sm:w-48 md:w-64"
+            onClick={() => setLightState(!lightState)}
+          />
         </div>
+
         <button
-          onClick={lightStateChange}
-          className="text-white border rounded p-2 m-4 sm:m-8"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setLightState(!lightState)}
         >
-          Change Light
+          {lightState ? "Turn Off" : "Turn On"}
         </button>
       </div>
     </>
